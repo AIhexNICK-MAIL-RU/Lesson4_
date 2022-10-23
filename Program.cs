e234d4fcf735c431d5594fcfbcd41dc04e1cc74f
VitaxaOv <vit2013aly@mail.ru>
2022-10-23 10:12:24 +0300

    Create project

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Задача 24: Напишите программу, которая принимает 
+//на вход число (А) и выдаёт сумму чисел от 1 до А.
+//7 -> 28 4 -> 10 8 -> 36
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int a);
+
+if(!isParsed)
+{
+    Console.WriteLine("Не правильно введены данные");
+    return;
+}
+if(a < 1)
+{
+    Console.WriteLine("0");
+    return;
+}
+
+int sum = GetSumNumbers(a);
+
+Console.WriteLine(sum);
+
+
+int GetSumNumbers(int number)
+{
+    int sum = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        sum+= i;
+    }
+
+    return sum;
+}
+
+
+
